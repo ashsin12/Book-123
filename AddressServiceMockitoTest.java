fead86e5a0e61db31388f7dd32ae5ab13dde2f51
ashsin12 <84857931+ashsin12@users.noreply.github.com>
2021-09-29 17:41:38 +0530

    Add files via upload

--- AddressServiceMockitoTest.java
+++ AddressServiceMockitoTest.java
@@ -44,6 +44,12 @@ import com.demo.book.exception.AddressNotFoundException;
 			void testGetAddressByAddressId() throws AddressNotFoundException {
 				
 				Address address = new Address();
+				address.setAddressId(3);
+				address.setAddress("hbyyhbfbn");
+				address.setCity("gggbg");
+				address.setCountry("Frr");
+				address.setPincode(55666);
+				
 				Mockito.when(addressDao.findById(3)).thenReturn(Optional.of(address));
 				Address std= addressService.getAddressByAddressId(3);
 				assertEquals(3, std.getAddressId());
@@ -56,7 +62,18 @@ import com.demo.book.exception.AddressNotFoundException;
 			@Test
 			void testGetAllAddress() {
 				Address std1 = new Address();
+				std1.setAddressId(3);
+				std1.setAddress("hbyyhbfbn");
+				std1.setCity("gggbg");
+				std1.setCountry("frr");
+				std1.setPincode(55666);
+				
 				Address std2 = new Address();
+				std2.setAddressId(3333);
+				std2.setAddress("hbyyhbfbn");
+				std2.setCity("gggbg");
+				std2.setCountry("frr");
+				std2.setPincode(55666);
 				//Address std2 = new Address(3333, "hbyyhbfbn","gggbg","frr",55666);
 				List<Address> addressList = new ArrayList<>();
 				addressList.add(std1);
@@ -70,24 +87,30 @@ import com.demo.book.exception.AddressNotFoundException;
 				assertEquals("frr", address.get(0).getCountry());
 				assertEquals(55666, address.get(0).getPincode());
 				assertEquals(3333, address.get(1).getAddressId());
-				assertEquals("hbyyhbfbn", address.get(0).getAddress());
-				assertEquals("gggbg", address.get(0).getCity());
-				assertEquals("frr", address.get(0).getCountry());
-				assertEquals(55666, address.get(0).getPincode());
+				assertEquals("hbyyhbfbn", address.get(1).getAddress());
+				assertEquals("gggbg", address.get(1).getCity());
+				assertEquals("frr", address.get(1).getCountry());
+				assertEquals(55666, address.get(1).getPincode());
 			}
 			
 			@Test
 			void testAddAddress() {
 				//Address address = new Address(3333, "hbyyhbfbn","gggbg","frr",55666);
 				Address address = new Address();
+				address.setAddressId(3333);
+				address.setAddress("hbyyhbfbn");
+				address.setCity("gggbg");
+				address.setCountry("Frr");
+				address.setPincode(55666);
+				
 				Mockito.when(addressDao.save(address)).thenReturn(address);
 				
 				Address newAddress = addressService.addAddress(address);
-				assertEquals(1023, newAddress.getAddressId());
-				assertEquals("Charan", newAddress.getAddress());
-				assertEquals("Chahhhran", newAddress.getCity());
-				assertEquals("Chahhhran", newAddress.getCountry());
-				assertEquals(567, newAddress.getPincode());
+				assertEquals(3333, newAddress.getAddressId());
+				assertEquals("hbyyhbfbn", newAddress.getAddress());
+				assertEquals("gggbg", newAddress.getCity());
+				assertEquals("Frr", newAddress.getCountry());
+				assertEquals(55666, newAddress.getPincode());
 			}
 			
 			/*Mockito
@@ -96,10 +119,17 @@ import com.demo.book.exception.AddressNotFoundException;
 			@Test
 			void testUpdateAddress() {
 				Address address = new Address();
+				
+				address.setAddressId(3);
+				address.setAddress("hbyyhbfbn");
+//				address.setCity("gggbg");
+//				address.setCountry("Frr");
+//				address.setPincode(55666);
+				
 				Mockito.when(addressDao.findById(3)).thenReturn(Optional.of(address));
 				Mockito.when(addressDao.save(address)).thenReturn(address);
 				Address updatedAddress = addressService.updateAddress(3, address);
-				assertEquals("yyy", updatedAddress.getAddress());
+				assertEquals("hbyyhbfbn", updatedAddress.getAddress());
 //				assertEquals("Chahhhran", updatedAddress.getCity());
 //				assertEquals("Chahhhran", updatedAddress.getCountry());
 //				assertEquals(567, updatedAddress.getPincode());
@@ -111,6 +141,13 @@ import com.demo.book.exception.AddressNotFoundException;
 			@Test
 			void testDeleteAddress() {
 				Address address = new Address();
+				
+				address.setAddressId(3);
+				address.setAddress("hbyyhbfbn");
+				address.setCity("gggbg");
+				address.setCountry("Frr");
+				address.setPincode(55666);
+				
 				Mockito.when(addressDao.findById(3)).thenReturn(Optional.of(address));
 				addressDao.deleteById(3);
 				Address std = addressService.deleteAddressByAddress(3);
